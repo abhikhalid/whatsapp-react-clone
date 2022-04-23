f0d454a22d60249b2c30c0483fb623fabfc2f183
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2022-04-23 19:47:52 +0600

    first-commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBT2R6LGKqHwIS7tBJu7GafZdLss-ztA5s",
+    authDomain: "whatsapp-react-clone-a5627.firebaseapp.com",
+    projectId: "whatsapp-react-clone-a5627",
+    storageBucket: "whatsapp-react-clone-a5627.appspot.com",
+    messagingSenderId: "936051231769",
+    appId: "1:936051231769:web:25dc38d64273c3854bae88"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
